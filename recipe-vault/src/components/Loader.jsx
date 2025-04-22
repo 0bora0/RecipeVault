2f5d3a0fe51b3fc468eaf68ee3d4f0d178505837
bora2211 <bora.nedzhib22@gmail.com>
2025-04-22 19:18:33 +0300

    update App.css

--- recipe-vault/src/components/Loader.jsx
+++ recipe-vault/src/components/Loader.jsx
@@ -1,14 +1,10 @@
-import { useLoading } from '../context/LoadingContext';
-import loadingGif from '../assets/loading.gif';
 import '../styles/Loader.css';
 
 export default function Loader() {
-  const { isLoading } = useLoading();
-  if (!isLoading) return null;
-
   return (
-    <div className="loader-overlay">
-      <img src={loadingGif} alt="Loading..." className="loader-gif" />
+    <div className="loader-container">
+      <div className="spinner"></div>
+      <p>Loading...</p>
     </div>
   );
 }
